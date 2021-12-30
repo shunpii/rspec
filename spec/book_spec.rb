a056c64526c58e012e49c2881c9f202b10a9ca0f
ShunyaSago <shun.s0029@gmail.com>
2021-12-30 00:58:24 +0000

    [Add]記述

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe Book,"モデルに関するテスト", type: :model do
             expect(FactoryBot.build(:book)).to be_valid
         end
     end
-    
+    context "空白のバリデーションチェック" do
+        it "titleが空白の場合にバリデーションチェクされ空白のエラーメッセージが帰ってきているか" do
+            book = Book.new(title:'', body:'hoge')
+            expect(book).to be_valid
+            expect(book.errors[:title]).to include("can't be blank")
+        end
+        it ""
+    end
 end
