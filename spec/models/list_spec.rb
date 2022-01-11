d60cdf73abd23edcaaf4004c003d941aab64aef3
ShunyaSago <shun.s0029@gmail.com>
2022-01-11 13:17:13 +0000

    [Finish]記述

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+
+#describe：モデルのテスト'
+#it：有効な投稿内容の場合は保存されるか
+#エクスペクテーション：データの作成
+#マッチャー：有効であるか
+
+
+describe 'モデルのテスト' do
+  it '有効な投稿内容の場合は保存されるか' do
+    expect(FactoryBot.build(:list)).to be_valid
+  end
+end
