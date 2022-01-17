d80df3fe311a782f42ab9753746c8c6d6260aab5
ShunyaSago <shun.s0029@gmail.com>
2022-01-17 13:39:43 +0000

    [add]コードの記述

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -10,8 +10,25 @@ require 'rails_helper'
       #it：トップ画面(top_path)に「ここはTopページです」が表示されているか
         #テストコード
       #it：top_pathが"/top"であるか
-        テストコード
+        #テストコード
 
+
+describe '投稿のテスト' do
+  let!(:list){ create(:list, title:'hoge',body:'body') }
+  describe 'トップ画面(top_path)のテスト' do
+    before do
+      visit top_path
+    end
+    context '表示の確認' do
+      it 'トップ画面(top_path)に「ここはTopページです」が表示されているか' do
+        expect(page).to have_context 'ここはTopページです'
+      end
+      it 'top_pathが"/top"であるか' do
+        expect(current_path).to eq('/top')
+      end
+    end
+  end
+end
 
 
 
